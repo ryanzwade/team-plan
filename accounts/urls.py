e443939481345c762867e9522ce9669edc4245ab
Ryan Wade <ryanzwade@hotmail.com>
2023-03-08 17:47:39 -0600

    17 done updates add

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,8 +4,8 @@ from accounts.views import user_login, user_logout, user_signup
 
 urlpatterns = [
 
-    path("login/", user_login, name="user_login"),
-    path("logout/", user_logout, name="user_logout"),
-    path("signup/", user_signup, name="user_signup"),
+    path("login/", user_login, name="login"),
+    path("logout/", user_logout, name="logout"),
+    path("signup/", user_signup, name="signup"),
 
 ]
