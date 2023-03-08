96cbfbbaa8f328ab4db50c394e40b4dc0f219c39
Ryan Wade <ryanzwade@hotmail.com>
2023-03-08 00:11:45 -0600

    Feature 14 some -1

--- projects/urls.py
+++ projects/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from projects.views import project_list, show_project
+from projects.views import project_list, show_project, create_project
 
 
 urlpatterns = [
+    path("create/", create_project, name="create_project"),
     path("<int:id>/", show_project, name="show_project"),
     path("", project_list, name="list_projects"),
     path("projects/", project_list),
--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect
 from projects.models import Project
 from django.contrib.auth.decorators import login_required
+from projects.forms import ProjectForm
 
 # Create your views here.
 
@@ -16,8 +17,27 @@ def project_list(request):
 
 @login_required
 def show_project(request, id):
-    project = get_object_or_404(Project, id=id)
+    project = Project.objects.get(id=id)
     context = {
-        "project_object": project,
+        "show_project": project,
     }
     return render(request, "projects/detail.html", context)
+
+
+@login_required
+def create_project(request):
+    if request.method == "POST":
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            project = form.save(False)
+            project.owner = request.user
+            project.save()
+            return redirect("list_projects")
+
+    else:
+        form = ProjectForm()
+
+    context = {
+        "form": form,
+    }
+    return render(request, "projects/create.html", context)
