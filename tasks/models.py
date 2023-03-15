153ce26480cf17ccfae146f5048d248ea7f3281b
Ryan Wade <ryanzwade@hotmail.com>
2023-03-15 15:47:15 -0500

    style changes

--- tasks/models.py
+++ tasks/models.py
@@ -5,8 +5,8 @@ from django.conf import settings
 # Create your models here.
 class Task(models.Model):
     name = models.CharField(max_length=200)
-    start_date = models.DateTimeField(null=True)
-    due_date = models.DateTimeField(null=True)
+    start_date = models.DateTimeField()
+    due_date = models.DateTimeField()
     is_completed = models.BooleanField(default=False)
     project = models.ForeignKey(
         "projects.Project",
